21a460922fde4c13164f12de30027cc675a833f4
Wu Jingyuan <katiewu0304@gmail.com>
2015-09-12 20:34:20 -0400

    fix bug

--- src/edu/upenn/cis573/hwk1/CrossValidation.java
+++ src/edu/upenn/cis573/hwk1/CrossValidation.java
@@ -1,14 +1,10 @@
 package edu.upenn.cis573.hwk1;
 
-import java.util.Iterator;
-
 import edu.upenn.cis573.Compare.CipherComparator;
 import edu.upenn.cis573.Compare.CompareResult;
-import edu.upenn.cis573.Decrypt.DecryptTool;
 import edu.upenn.cis573.Decrypt.Decrypter;
 import edu.upenn.cis573.DecryptModel.DecryptModel;
 import edu.upenn.cis573.DecryptModel.FrequencyModel;
-import edu.upenn.cis573.DecryptModel.FrequencyResult;
 import edu.upenn.cis573.Encrypt.Encrypter;
 import edu.upenn.cis573.Encrypt.SimpleEncrypter;
 import edu.upenn.cis573.Input.Corpus;
@@ -21,15 +17,10 @@ public class CrossValidation {
 	
 	private Input testInput;
 	private Corpus<Input> corpus;
-	private FrequencyResult testResult;
-	private FrequencyResult corpusResult;
-	private char[] mapping;
 	
 	public CrossValidation(Input testInput, Corpus<Input> corpus){
 		this.testInput = testInput;
 		this.corpus = corpus;
-		testResult = new FrequencyResult();
-		corpusResult = new FrequencyResult();
 	}
 	
 	public CompareResult run(){
@@ -50,27 +41,6 @@ public class CrossValidation {
 		CipherComparator comparator = CipherComparator.getCipherComparator();
 		CompareResult result = comparator.compare(testInput, testDecryptFile);
 		return result;
-	}
-	
-	private void generateEncryptResult(Input input){
-		testResult.analyzeInput(input);
-		testResult.sort();
-//		System.out.println(testResult);
-	}
-	
-	private void generateCorpusResult(){
-		Iterator<Input> iterator = corpus.iterator();
-		while(iterator.hasNext()){
-			Input input = iterator.next();
-			corpusResult.analyzeInput(input);
-		}
-		corpusResult.sort();
-//		System.out.println(corpusResult);
-	}
-	
-	private void mappingResult(){
-		mapping = corpusResult.mapping(testResult);
-//		System.out.println(mapping);
 	}
 	
 }
